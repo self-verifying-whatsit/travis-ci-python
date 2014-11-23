ce7e2518401a485b9b79c6ea57c7dcf74415263c
Oliver Bestwalter <oliver@bestwalter.de>
2014-11-24 00:04:16 +0100

    remove mktmpenv call (it's a function)

--- svb.sh
+++ svb.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-mktmpenv
-git clone git@github.com:self-validating-blueprint/travis-ci-python.git .
+git clone git@github.com:self-validating-blueprint/travis-ci-python.git travis-ci-python
 pip install -r requirements.txt
 py.test
